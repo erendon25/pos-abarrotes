f560d4cbc9274718122e24fc7d3c533aeb0a4821
erendon25 <erickrendon18@gmail.com>
2026-01-05 23:35:37 -0500

    Update Vite configuration to specify build output directory and assets location; minor adjustment to deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/pos-abarrotes/', // Si tu repositorio es username.github.io, usa '/'. Si es username.github.io/repo-name, usa '/repo-name/'
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
