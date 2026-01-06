039970c317d876e906f817109d075efcd291f979
erendon25 <erickrendon18@gmail.com>
2026-01-05 23:27:18 -0500

    cambios

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Si tu repositorio es username.github.io, usa '/'. Si es username.github.io/repo-name, usa '/repo-name/'
+  base: '/pos-abarrotes/', // Si tu repositorio es username.github.io, usa '/'. Si es username.github.io/repo-name, usa '/repo-name/'
 })
